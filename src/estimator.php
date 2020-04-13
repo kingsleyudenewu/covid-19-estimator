977d105c63d7acdd9f140ddfb1086006d5a33e08
Kingsley Udenewu <kingzpacking@gmail.com>
2020-04-13 23:59:19 +0100

    Add the new payload to be returned

--- src/estimator.php
+++ src/estimator.php
@@ -2,5 +2,30 @@
 
 function covid19ImpactEstimator($data)
 {
-  return $data;
+    if (!is_array($data) || empty($data)) {
+        return false;
+    }
+
+    $estimatorData = [];
+    $estimatorData['data'] = $data;
+    $estimatorData['impact'] = [
+        'currentlyInfected' => (int)$data['reportedCases'] * 10,
+        'infectionsByRequestedTime' => (int) (($data['reportedCases'] * 10) * 512),
+        'severeCasesByRequestedTime' => (int) ((($data['reportedCases'] * 10) * 512) * 0.15),
+        'hospitalBedsByRequestedTime' => (int) ($data['totalHospitalBeds'] -  ((($data['reportedCases'] * 10) * 512) * 0.15)),
+        'casesForICUByRequestedTime' => (int) ((($data['reportedCases'] * 10) * 512) * 0.15),
+        'casesForVentilatorsByRequestedTime' => (int) ((($data['reportedCases'] * 10) * 512) * 0.02),
+        'dollarsInFlight' => (int) (((($data['reportedCases'] * 10) * 512) * 0.65 * 1.5) / 30)
+    ];
+    $estimatorData['severeImpact'] = [
+        'currentlyInfected' => (int)$data['reportedCases'] * 50,
+        'infectionsByRequestedTime' => (int) (($data['reportedCases'] * 50) * 512),
+        'severeCasesByRequestedTime' => (int) ((($data['reportedCases'] * 50) * 512) * 0.15),
+        'hospitalBedsByRequestedTime' => (int) ($data['totalHospitalBeds'] - ((($data['reportedCases'] * 50) * 512) * 0.15)),
+        'casesForICUByRequestedTime' => (int) ((($data['reportedCases'] * 50) * 512) * 0.05),
+        'casesForVentilatorsByRequestedTime' => (int) ((($data['reportedCases'] * 50) * 512) * 0.02),
+        'dollarsInFlight' => (int) (((($data['reportedCases'] * 50) * 512) * 0.65 * 1.5) / 30)
+    ];
+
+    return $estimatorData;
 }
